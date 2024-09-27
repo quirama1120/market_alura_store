f603cb28da624e7c61ca53878b909b2fe397c855
Christian Quirama <quirama1120@gmail.com>
2024-09-27 13:41:16 -0500

    replacing folders

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,74 @@
+import ProductModels.Product;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner keyword = new Scanner(System.in);
+        boolean running = true;
+        System.out.println("Ingresa el valor de tu tarjeta de crédito");
+        int targetValue = keyword.nextInt();
+        List<Product> productList = new ArrayList<>();
+
+        while(running) {
+            System.out.println("""
+                    Escoge una de las siguientes opciones:
+                    0. Si deseas salir.
+                    1. Si deseas hacer compras.
+                    2. Si deseas consultar tu saldo.
+                    3. Si quieres consultar productos.
+                    
+                    """);
+
+            int userInput = keyword.nextInt();
+            switch (userInput) {
+                case 0 -> {
+                    System.out.println("Has comprado" + productList);
+                    System.out.println("tu saldo es " + targetValue);
+                    System.out.println("exiting...");
+                    running = false;
+                }
+                case 1 -> {
+                    System.out.println("Escribe el nombre del producto que deseas comprar");
+                    keyword.nextLine();
+                    String productName = keyword.nextLine();
+                    System.out.println("Ingresa el valor del producto comprado");
+                    int productPrice = keyword.nextInt();
+                    keyword.nextLine();
+
+                    if(productPrice > targetValue) {
+                        System.out.println("************************");
+                        System.out.println("No tienes suficiente saldo");
+                        Collections.sort(productList);
+                        System.out.println("Tus compras fueron " + productList);
+                        System.out.println("Tu saldo es " + targetValue);
+                        System.out.println("Exiting...");
+                        running = false;
+                    } else {
+                            Product product = new Product(productName, productPrice);
+                            productList.add(product);
+                            System.out.println("***********************");
+                            Collections.sort(productList);
+                            System.out.println("Productos en la canasta " + productList);
+                            targetValue -= productPrice;
+                            System.out.println("Tu saldo restante es " + targetValue);
+
+                    }
+                }
+                case 2 -> {
+                    System.out.println("Tu saldo es " + targetValue);
+                }
+                case 3 -> {
+                    System.out.println("Has comprado los siguientes productos: " + productList);
+                }
+                default -> {
+                    System.out.println("Has ingresado una opción que no es válida");
+                }
+            }
+        }
+
     }
 }
--- src/ProductModels/Product.java
+++ src/ProductModels/Product.java
@@ -1,4 +1,34 @@
 package ProductModels;
 
-public class Product {
+import java.util.Comparator;
+
+public class Product implements Comparable <Product> {
+    private final String productName;
+    private final int price;
+
+
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public Product(String productName, int price) {
+        this.productName = productName;
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return  this.getProductName() + " con un valor de ($" + this.getPrice() + ")";
+    }
+
+
+    @Override
+    public int compareTo(Product nextProduct) {
+        return Integer.compare(this.price, nextProduct.price);
+    }
 }
